7ffab240d421987f113bd364e8437e643c812959
Unknown <vishal_vijayan@outlook.com>
2018-12-15 11:38:32 +0530

    Revert "Added Unique Id for Products"
    This reverts commit 1c8d049898ff8597d563b0658713d0c0e36750ce.

--- models/product.js
+++ models/product.js
@@ -1,39 +1,48 @@
 const fs = require('fs');
 const path = require('path');
 
-const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
 
-
-const getProductsFromFile = (callback) => {
-    fs.readFile(p, (err, data) => {
-        if (err) {
-            callback([]);
-        } else {
-            callback(JSON.parse(data));
-        }
-    })
-}
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
 
 module.exports = class Product {
-    constructor(title, imageUrl, description, price) {
-        this.title = title;
-        this.imageUrl = imageUrl;
-        this.description = description;
-        this.price = price;
-    }
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
 
-    save() {
-        this.id = Math.random().toString();
-        getProductsFromFile(products => {
-            products.push(this);
-            fs.writeFile(p, JSON.stringify(products), err => {
-                console.log(err);
-            })
-        });
-    }
+  save() {
+    this.id = Math.random().toString();
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
 
-    static fetchAll(callback) {
-        getProductsFromFile(callback);
-    }
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
 
-}
+  static findById(id, cb) {
+    getProductsFromFile(products => {
+      const product = products.find(p => p.id === id);
+      cb(product);
+    });
+  }
+};
