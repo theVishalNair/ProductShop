52ae75fb0c0fe8856323e7251e00b21659ff86c1
Unknown <vishal_vijayan@outlook.com>
2018-12-30 23:26:08 +0530

    Added Cart functionalities and other related information

--- models/cart.js
+++ models/cart.js
@@ -11,7 +11,10 @@ module.exports = class Cart {
   static addProduct(id, productPrice) {
     // Fetch the previous cart
     fs.readFile(p, (err, fileContent) => {
-      let cart = { products: [], totalPrice: 0 };
+      let cart = {
+        products: [],
+        totalPrice: 0
+      };
       if (!err) {
         cart = JSON.parse(fileContent);
       }
@@ -23,18 +26,55 @@ module.exports = class Cart {
       let updatedProduct;
       // Add new product/ increase quantity
       if (existingProduct) {
-        updatedProduct = { ...existingProduct };
+        updatedProduct = { ...existingProduct
+        };
         updatedProduct.qty = updatedProduct.qty + 1;
         cart.products = [...cart.products];
         cart.products[existingProductIndex] = updatedProduct;
       } else {
-        updatedProduct = { id: id, qty: 1 };
+        updatedProduct = {
+          id: id,
+          qty: 1
+        };
         cart.products = [...cart.products, updatedProduct];
       }
       cart.totalPrice = cart.totalPrice + +productPrice;
       fs.writeFile(p, JSON.stringify(cart), err => {
         console.log(err);
       });
+    });
+
+  }
+  static deleteProduct(id, productPrice) {
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        return;
+      }
+      const updatedCart = { ...JSON.parse(fileContent)
+      };
+      const product = updatedCart.products.find(prod => prod.id === id);
+      if (!product) {
+        return;
+      }
+      const productQty = product.qty;
+      updatedCart.products = updatedCart.products.filter(prod => prod.id !== id);
+      updatedCart.totalPrice = updatedCart.totalPrice - (productPrice * productQty);
+
+      fs.writeFile(p, JSON.stringify(updatedCart), err => {
+        console.log(err);
+      })
+    });
+  }
+
+  static getCart(cb) {
+    fs.readFile(p, (err, fileContent) => {
+      const cart = JSON.parse(fileContent);
+      if (err) {
+        cb(null);
+      } else {
+
+        cb(cart);
+      }
     });
   }
 };
